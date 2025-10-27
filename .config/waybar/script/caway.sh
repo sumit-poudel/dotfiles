4bab1434d6568ca3dab41bbcd7ba6ccaf92e3352
Sumit Poudel <sumitpoudel325@gmail.com>
2025-10-27 19:38:49 +0545

    minor updates

--- .config/waybar/script/caway.sh
+++ .config/waybar/script/caway.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-
 bar="▁▂▃▄▅▆▇█"
 dict="s/;//g;"
 
-# Build sed dictionary
 i=0
 while [ $i -lt ${#bar} ]; do
     dict="${dict}s/$i/${bar:$i:1} /g;"
@@ -12,9 +10,8 @@ done
 dict="${dict}s/ $//;"
 
 bars=8
-config_file="/tmp/polybar_cava_config_$$"  # Unique per run
+config_file="/tmp/polybar_cava_config_$$"
 
-# Create config
 cat > "$config_file" <<EOF
 [general]
 bars = $bars
@@ -26,14 +23,12 @@ data_format = ascii
 ascii_max_range = 7
 EOF
 
-# Cleanup on exit
 cleanup() {
     rm -f "$config_file"
-    pkill -P $$ cava 2>/dev/null  # Kill child cava if still alive
 }
 trap cleanup EXIT
 
-# Run cava and output bars
+killall cava
 cava -p "$config_file" | while read -r line; do
     echo "$line" | sed "$dict"
 done
